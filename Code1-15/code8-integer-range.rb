df4cdbd92a2e7ddb5a2c52235a3fa6281ed3357f
MansiJPawar <mansipawar517@gmail.com>
2022-01-04 14:49:59 +0530

    Code1-15 , adding code8 modified

--- Code1-15/code8-integer-range.rb
+++ Code1-15/code8-integer-range.rb
@@ -1,5 +1,10 @@
 #Ruby program to check two integer values whether either of them is in the range 20..30 inclusive.
+#better
 puts "enter number"
 num = gets.chomp().to_i
+
+puts "enter number2"
+num2 = gets.chomp().to_i
+
 #by include and range method in one statement
-puts (20..30).include?(num)
+puts (20..30).include?(num) || (20..30).include?(num2)
