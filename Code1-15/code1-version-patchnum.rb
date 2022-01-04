3a9b80e211ac770fe63811b0e8535737e1e96298
MansiJPawar <mansipawar517@gmail.com>
2022-01-04 11:45:15 +0530

    Code1-15 , adding code1

---
+++ Code1-15/code1-version-patchnum.rb
@@ -0,0 +1,8 @@
+#Ruby program to get ruby version with patch number
+
+puts "Code to print ruby version and ruby patch number"
+puts
+puts (RUBY_VERSION + "  Ruby Version")
+puts
+puts (RUBY_PATCHLEVEL.to_s + "      Patch Number of Ruby")
+puts
