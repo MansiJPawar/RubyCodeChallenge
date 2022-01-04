f0e09448820f2be9feb353ef1b3de7b19b7d21f6
MansiJPawar <mansipawar517@gmail.com>
2022-01-04 12:00:18 +0530

    Code1-15 , adding code2

---
+++ Code1-15/code2-date-time.rb
@@ -0,0 +1,12 @@
+# Ruby program to display the current date and time
+
+time = Time.new
+
+#time
+T = time.strftime("%I:%M %p") 
+#date
+D = time.strftime("%d of %B, %Y") 
+#day of week
+W = time.strftime("%A") 
+ 
+puts " Today its #{W} \n Date is #{D} \n Time is #{T}."
