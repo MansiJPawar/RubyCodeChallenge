bf2325ce966ec201dfe16fe1d42414161e9b8966
MansiJPawar <mansipawar517@gmail.com>
2022-01-04 12:32:44 +0530

    Code1-15 , adding code4

---
+++ Code1-15/code4-circle-area-perimeter.rb
@@ -0,0 +1,9 @@
+#Area and Parimeter of Circle to given Radius of circle
+puts "enter circle radius"
+r = gets.chomp().to_f
+
+#power function is used r**2 = r square
+area = 3.14*r**2
+parimeter = 2*3.14*r**2
+
+puts " Area of Circle is #{area}\n Parimeter of Circle is #{parimeter}"
